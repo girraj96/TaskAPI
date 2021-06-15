229fad9aa7f665ca2844c3bc8756fe9fd7e58a62
admin <admin@admins-iMac.local>
2021-06-15 11:15:54 +0530

    main branch cloned

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default class App extends Component {
         <Provider store={store}>
           <Routes />
         </Provider>
-        <FlashMessage position="top" />
+        <FlashMessage />
       </>
     )
   }
