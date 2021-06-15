1ac3e11828344aa22d191b5790916712911660fb
admin <admin@admins-iMac.local>
2021-06-15 11:39:44 +0530

    flash message position removed

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default class App extends Component {
         <Provider store={store}>
           <Routes />
         </Provider>
-        <FlashMessage position="top" />
+        <FlashMessage  />
       </>
     )
   }
